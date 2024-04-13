364626778c8f587e968c590879da562b648a3ce8
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-13 07:49:43 -0500

    feat: updates table component

--- src/app/page/1/page.tsx
+++ src/app/page/1/page.tsx
@@ -3,6 +3,12 @@ import Button from "@/components/Button";
 import Table from "@/components/Table";
 
 export default function PageOne() {
+  const data = [
+    ["John Doe", "25", "USA"],
+    ["Jane Doe", "22", "UK"],
+    ["Sam Smith", "32", "Canada"],
+  ];
+
   return (
     <div>
       <h1 className="font-bold font-heading text-slate-900 text-3xl">
@@ -15,33 +21,7 @@ export default function PageOne() {
       </p>
 
       <div className="flex justify-center mt-16">
-        <Table tableHeadings={["Name", "Age", "Country"]} data={[[], [], []]} />
-        {/* <table className="table-auto text-slate-900 border-2 border-slate-900 rounded-md border-spacing-0 border-separate">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Name</th>
-              <th className="px-4 py-2">Age</th>
-              <th className="px-4 py-2">Country</th>
-            </tr>
-          </thead>
-          <tbody className="border border-slate-900">
-            <tr>
-              <td className="border px-4 py-2">John Doe</td>
-              <td className="border px-4 py-2">25</td>
-              <td className="border px-4 py-2">USA</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Jane Doe</td>
-              <td className="border px-4 py-2">22</td>
-              <td className="border px-4 py-2">UK</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Sam Smith</td>
-              <td className="border px-4 py-2">32</td>
-              <td className="border px-4 py-2">Canada</td>
-            </tr>
-          </tbody>
-        </table> */}
+        <Table headings={["Name", "Age", "Country"]} data={data} />
       </div>
 
       <div className="flex justify-end mt-16">
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,40 +1,32 @@
 import { FC } from "react";
 
 type TableProps = {
-  tableHeadings: string[];
+  headings: string[];
   data: string[][];
 };
 
-const Table: FC<TableProps> = ({ tableHeadings }) => {
+const Table: FC<TableProps> = ({ headings, data }) => {
   return (
     <table className="table-auto text-slate-900 border-2 border-slate-900 rounded-md border-spacing-0 border-separate">
-      <thead>
+      <thead className="bg-slate-100 rounded-t-md">
         <tr>
-          {tableHeadings.map((heading, index) => {
-            return (
-              <th key={index} className="px-4 py-2">
-                {heading}
-              </th>
-            );
-          })}
+          {headings.map((heading, index) => (
+            <th key={index} className="px-4 py-2">
+              {heading}
+            </th>
+          ))}
         </tr>
       </thead>
-      <tbody className="border border-slate-900">
-        <tr>
-          <td className="border px-4 py-2">John Doe</td>
-          <td className="border px-4 py-2">25</td>
-          <td className="border px-4 py-2">USA</td>
-        </tr>
-        <tr>
-          <td className="border px-4 py-2">Jane Doe</td>
-          <td className="border px-4 py-2">22</td>
-          <td className="border px-4 py-2">UK</td>
-        </tr>
-        <tr>
-          <td className="border px-4 py-2">Sam Smith</td>
-          <td className="border px-4 py-2">32</td>
-          <td className="border px-4 py-2">Canada</td>
-        </tr>
+      <tbody className="">
+        {data.map((row, index) => (
+          <tr key={index}>
+            {row.map((cell, index) => (
+              <td key={index} className="px-4 py-2">
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
