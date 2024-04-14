00a21735e8d690dc7418f931faa26ad05fa60cea
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-14 09:52:02 -0500

    feat: udpates barchart styles and layout

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,17 +1,69 @@
 "use client";
-import { VictoryChart, VictoryBar, VictoryTheme } from "victory";
+import colors from "tailwindcss/colors";
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryTheme,
+  VictoryThemeDefinition,
+  VictoryAxis,
+  VictoryLabel,
+} from "victory";
 
 const BarChart = () => {
-  const sampleData = [
-    { x: "A", y: 2 },
-    { x: "B", y: 3 },
-    { x: "C", y: 5 },
-    { x: "D", y: 4 },
+  const data = [
+    { x: "Biff", y: 2, fill: colors.blue[500] },
+    { x: "Tina", y: 3, fill: colors.green[500] },
+    { x: "Egon", y: 5, fill: colors.red[500] },
+    { x: "Whitney", y: 4, fill: colors.yellow[400] },
   ];
 
+  console.log("victorytheme: ", VictoryTheme.material);
+
+  const theme: VictoryThemeDefinition = {
+    axis: {
+      style: {
+        tickLabels: {
+          fill: colors.slate[900],
+        },
+
+        axis: {
+          stroke: colors.slate[900],
+        },
+        grid: {
+          stroke: "transparent",
+        },
+      },
+    },
+    bar: {
+      style: {
+        data: {
+          fill: colors.blue[500],
+          width: 40,
+        },
+      },
+    },
+  };
+
   return (
-    <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
-      <VictoryBar style={{ data: { fill: "#c43a31" } }} data={sampleData} />
+    <VictoryChart theme={theme} domainPadding={30}>
+      <VictoryAxis
+        dependentAxis
+        tickLabelComponent={<VictoryLabel dx={-10} />}
+        tickFormat={(tick: number) => `${Math.round(tick)}`}
+      />
+      <VictoryAxis tickLabelComponent={<VictoryLabel dy={10} />} />
+      <VictoryBar
+        animate={{
+          duration: 2000,
+          onLoad: { duration: 1000 },
+        }}
+        style={{
+          data: {
+            fill: ({ datum }) => datum.fill,
+          },
+        }}
+        data={data}
+      />
     </VictoryChart>
   );
 };
