2d288d862eb731c6fa43b176a78fd73dda5314cb
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-13 19:15:11 -0500

    feat: updates table styles

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,21 +7,21 @@ type TableProps = {
 
 const Table: FC<TableProps> = ({ headings, data }) => {
   return (
-    <table className="bg-blue-100 table-auto text-slate-900 border-2 border-slate-900 rounded-md border-spacing-0 border-separate">
-      <thead className="rounded-t-md">
+    <table className="table-auto text-slate-900 border-2 border-slate-900 rounded-md border-spacing-0 border-separate overflow-hidden">
+      <thead className="bg-slate-100 border-slate-900">
         <tr>
           {headings.map((heading, index) => (
-            <th key={index} className="px-4 py-2">
+            <th key={index} className="px-4 py-2 border-b">
               {heading}
             </th>
           ))}
         </tr>
       </thead>
-      <tbody className="">
+      <tbody>
         {data.map((row, index) => (
           <tr key={index}>
             {row.map((cell, index) => (
-              <td key={index} className="px-4 py-2">
+              <td key={index} className="px-4 py-2 border">
                 {cell}
               </td>
             ))}
