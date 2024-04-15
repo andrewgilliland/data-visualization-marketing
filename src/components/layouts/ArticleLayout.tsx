be4c4fd40ff9a3bba30f2fae21dbbdf4a2afa1f5
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-14 23:16:11 -0500

    feat: wip gamepad api

--- src/components/layouts/ArticleLayout.tsx
+++ src/components/layouts/ArticleLayout.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Button from "../Button";
+import { useRouter } from "next/navigation";
+import { debounce } from "lodash";
 
 type BaseLayoutProps = {
   title: string;
@@ -9,6 +12,70 @@ type BaseLayoutProps = {
 };
 
 const ArticleLayout: FC<BaseLayoutProps> = ({ title, children, page }) => {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const router = useRouter();
+
+  function pollGamepads() {
+    // Grab a list of gamepads that are currently connected or a empty array
+    const gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
+    // console.log(gamepads);
+
+    const pressedButtons = [];
+
+    // Loop through all gamepads connect to the computer
+    for (let g = 0; g < gamepads.length; g++) {
+      const gp = gamepads[g];
+      if (!!gp) {
+        // Loop through all gamepad buttons pick out pressed ones
+        for (let b = 0; b < gp.buttons.length; b++) {
+          if (gp.buttons[b].pressed) {
+            pressedButtons.push(b);
+          }
+        }
+      }
+    }
+
+    // console.log(`Currently pressing: ${pressedButtons.join(" + ")}`);
+    // debounce(pressedButton(pressedButtons, 0), 1000);
+    // pressedButton(pressedButtons, 0);
+
+    if (pressedButtons.join(" + ") === "0") {
+      console.log("Pressed button 0");
+      // console.log("pageNumber: ", pageNumber + 1);
+      setPageNumber((prev) => prev + 1);
+      console.log(`/page/${pageNumber}`);
+      router.push(`/page/${pageNumber}`);
+    }
+  }
+
+  const pressedButton = (pressedButtons: number[], button: number): any => {
+    if (pressedButtons.join(" + ") === "0") {
+      console.log(`Pressed button ${button}`);
+      console.log("page: ", page + 1);
+      // router.push(`/page/${page + 1}`);
+    }
+  };
+
+  useEffect(() => {
+    console.log("useEffect page: ----------------------------: ", pageNumber);
+
+    window.addEventListener("gamepadconnected", function (e) {
+      const gamepad = e.gamepad;
+      console.log(`Gamepad connected at index ${gamepad.index}: ${gamepad.id}.
+      ${gamepad.buttons.length} buttons, ${gamepad.axes.length} axes.`);
+
+      let interval;
+
+      if (!("ongamepadconnected" in window)) {
+        // No gamepad events available, poll instead.
+        interval = setInterval(pollGamepads, 300);
+      }
+    });
+
+    return () => window.removeEventListener("gamepadconnected", pollGamepads);
+  }, [pageNumber]);
+
   return (
     <div className="">
       <h1 className="font-bold font-heading text-slate-900 text-3xl">
