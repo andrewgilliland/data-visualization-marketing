5034b251c4e0174b8ecbebea2b92e356f80ac94e
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-16 12:39:15 -0500

    feat: updates piechart animation timing again again

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -55,7 +55,7 @@ const PieChart: FC<PieChartProps> = ({
       innerRadius={graphicInnerRadius}
       colorScale={colorScale}
       animate={{
-        duration: 1000,
+        onLoad: { duration: 1000 },
       }}
       style={{
         data: {
