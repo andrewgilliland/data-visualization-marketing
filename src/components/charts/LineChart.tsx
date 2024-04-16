a466d105667d366534ef1847dc76147844a288c5
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-15 21:32:24 -0500

    feat: updates linechart

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -15,20 +15,47 @@ type LineChartProps = {
 };
 
 const LineChart: FC<LineChartProps> = () => {
-  const data = [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 },
-    { x: 5, y: 7 },
-  ];
-
-  const data2 = [
-    { x: 1, y: 3 },
-    { x: 2, y: 4 },
-    { x: 3, y: 3 },
-    { x: 4, y: 5 },
-    { x: 5, y: 8 },
+  const dataset = [
+    {
+      data: [
+        { x: 1, y: 2 },
+        { x: 2, y: 3 },
+        { x: 3, y: 5 },
+        { x: 4, y: 4 },
+        { x: 5, y: 7 },
+      ],
+      stroke: colors.blue[500],
+    },
+    {
+      data: [
+        { x: 1, y: 3 },
+        { x: 2, y: 4 },
+        { x: 3, y: 3 },
+        { x: 4, y: 5 },
+        { x: 5, y: 8 },
+      ],
+      stroke: colors.red[500],
+    },
+    {
+      data: [
+        { x: 1, y: 4 },
+        { x: 2, y: 5 },
+        { x: 3, y: 6 },
+        { x: 4, y: 5 },
+        { x: 5, y: 6 },
+      ],
+      stroke: colors.green[500],
+    },
+    {
+      data: [
+        { x: 1, y: 2 },
+        { x: 2, y: 6 },
+        { x: 3, y: 7 },
+        { x: 4, y: 3 },
+        { x: 5, y: 5 },
+      ],
+      stroke: colors.yellow[400],
+    },
   ];
 
   return (
@@ -42,21 +69,17 @@ const LineChart: FC<LineChartProps> = () => {
         tickLabelComponent={<VictoryLabel dy={10} />}
         tickFormat={(tick: number) => `${Math.round(tick)}`}
       />
-      <VictoryLine
-        animate={{
-          duration: 2000,
-          onLoad: { duration: 1000 },
-        }}
-        data={data}
-      />
-      <VictoryLine
-        style={{ data: { stroke: colors.red[500] } }}
-        animate={{
-          duration: 2000,
-          onLoad: { duration: 1000 },
-        }}
-        data={data2}
-      />
+      {dataset.map(({ data, stroke }, index) => (
+        <VictoryLine
+          key={index}
+          style={{ data: { stroke } }}
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 },
+          }}
+          data={data}
+        />
+      ))}
     </VictoryChart>
   );
 };
