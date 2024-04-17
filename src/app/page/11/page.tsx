e874f9a75c9022ceb821ad60ec8e33a6132b7886
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-17 09:19:17 -0500

    feat: updates pageeleven

--- src/app/page/11/page.tsx
+++ src/app/page/11/page.tsx
@@ -2,29 +2,40 @@ import ArticleLayout from "@/components/layouts/ArticleLayout";
 import Text from "@/components/Text";
 import BarChart from "@/components/charts/BarChart";
 import LineChart from "@/components/charts/LineChart";
-import PieChart from "@/components/charts/PieChart";
+import Map from "@/components/maps/Map";
+
+import Heading from "@/components/Heading";
 
 export default function PageEleven() {
   return (
     <ArticleLayout title="Data Visualization Best Practices" page={11}>
-      <div className="flex h-60">
+      <div className="grid grid-cols-3 items-center h-60">
         <BarChart />
-        <PieChart />
+        <Map />
         <LineChart />
       </div>
       <div className="mt-4">
-        <Text>
-          Data visualization is a representation of data and information through
-          the use of graphics.
-        </Text>
-        <Text className="mt-2">
-          Charts, graphs, plots, maps, infographics are all types of data
-          visualizations.
-        </Text>
-        <Text className="mt-2">
-          These visual displays of data help communicate data relationships in a
-          way that is easy to digest.
-        </Text>
+        <div className="mt-8">
+          <Heading sectionLevel="h2">Know Your Audience</Heading>
+          <Text>
+            Understand who your audience is and what they are looking for. Make
+            sure the data visualization is easy to understand for them.
+          </Text>
+        </div>
+        <div className="mt-8">
+          <Heading sectionLevel="h2">Choose The Right Visualization</Heading>
+          <Text className="mt-2">
+            Choose the right visualization for your data. Make sure it is
+            appropriate for the data you are trying to represent.
+          </Text>
+        </div>
+        <div className="mt-8">
+          <Heading sectionLevel="h2">Keep It Simple</Heading>
+          <Text className="mt-2">
+            Keep the data visualization simple and easy to understand. Avoid too
+            many colors, labels, and data points.
+          </Text>
+        </div>
       </div>
     </ArticleLayout>
   );
